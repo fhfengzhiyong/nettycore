dacb2871e10eca527201b714b150e008d961eb23
fengzhiyong <1107665984@qq.com>
2017-08-03 19:14:31 +0800

    sd

--- chapter1/src/main/java/com/straw/nettycore/observer/Subject.java
+++ chapter1/src/main/java/com/straw/nettycore/observer/Subject.java
@@ -1,7 +1,13 @@
 package com.straw.nettycore.observer;
 
 /**
- * Created by Administrator on 7/30/2017.
+ * Created by fengzy on 7/30/2017.
+ * 主题接口
+ * 这里我们要实现一个订阅和通知功能的程序，客户端可以自由的订阅和退订，
+ * 这里说的让客户端可以随时都能增加或者减少，可以理解为在使用我们设计的这个订阅通知模式时。
  */
 public interface Subject {
+    public boolean register(Client client);
+    public boolean remove(Client client);
+    public void notification(String mes);
 }
