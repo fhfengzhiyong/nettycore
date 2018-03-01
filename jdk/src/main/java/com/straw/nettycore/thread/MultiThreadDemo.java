2e59f07528fd58dd6e19f81974deb170cb9133f8
fhfengzhiyong <1107665984@qq.com>
2018-03-01 17:36:29 +0800

    add file
    add thread

---
+++ jdk/src/main/java/com/straw/nettycore/thread/MultiThreadDemo.java
@@ -0,0 +1,60 @@
+package com.straw.nettycore.thread;
+
+import java.util.concurrent.locks.Lock;
+
+/**
+ * @author fengzy
+ * @date 3/1/2018
+ */
+public class MultiThreadDemo {
+    public static class Number{
+        private int count;
+        Object object=new Object();
+
+        /**
+         * count 属于类的变量,不能保证
+         * @throws InterruptedException
+         */
+        synchronized void increase () throws InterruptedException {
+            System.out.println("in ");
+           count=10;
+           Thread.sleep(1000);
+           System.out.println("increase value:"+count);
+            System.out.println("out");
+        }
+        public  void decrease () throws InterruptedException {
+            System.out.println("2 in ");
+            count=-10;
+            Thread.sleep(1000);
+            System.out.println("decrease value:"+count);
+            System.out.println("2 out");
+        }
+    }
+
+    public static void main(String[] args) {
+        final Number number = new Number();
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    number.increase();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    number.decrease();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+        thread1.start();
+    }
+}
